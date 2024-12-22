60aa7946ad415c7b1f863e33aae9735464a5631a
Akshay <52096313+AkshayGadepally@users.noreply.github.com>
2024-12-23 01:03:57 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,20 +1,69 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-        // System.out.print("$ ");
+      
 
         Scanner scanner = new Scanner(System.in);
-        while(true){
+
+        // while loop to start the shell with $ sign
+        while(true){        
         System.out.print("$ ");
         String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
-            if(input.isEmpty()){
-                System.out.print("$ ");
+        
+        if(input.startswith("type")){
+            if (input.equals("exit 0")) {
                 break;
             }
-    }
-    scanner.close();
+    
+            // this statement is for echo command 
+            if(input.contains("echo")){
+                String[] Array = input.split(" ");
+                for(int i = 1; i < Array.length ; i++){
+                    if(i == Array.length - 1){
+                        System.out.print(Array[i]);
+                        System.out.println();
+                        
+                    }
+                    else{
+                     System.out.print(Array[i] + " ");
+                    }
+                }
+                continue;
+             }
+        }
+        
+        // this statement is to exit when the user types "exit 0"
+        if (input.equals("exit 0")) {
+            break;
+        }
+
+        // this statement is for echo command 
+        if(input.startsWith("echo")){
+            String[] Array = input.split(" ");
+            for(int i = 1; i < Array.length ; i++){
+                if(i == Array.length - 1){
+                    System.out.print(Array[i]);
+                    System.out.println();
+                    
+                }
+                else{
+                 System.out.print(Array[i] + " ");
+                }
+            }
+            continue;
+         }
+
+        // this statement is to print for invalid commands
+        System.out.println(input + ": command not found");
+        if(input.equals(null)){
+            System.out.println("$ ");
+            break;
+         }
+        
+    }   
+        
+        scanner.close();
     }
 }
