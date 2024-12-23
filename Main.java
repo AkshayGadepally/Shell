ae5537d182f8ee72c772510c5b72e9292f9bdc76
Akshay <52096313+AkshayGadepally@users.noreply.github.com>
2024-12-23 23:59:09 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -11,27 +11,22 @@ public class Main {
         while(true){        
         System.out.print("$ ");
         String input = scanner.nextLine();
+
+        String[] command = {"type", "echo", "exit 0"};
         
-        if(input.startswith("type")){
-            if (input.equals("exit 0")) {
+        if(input.startsWith("type")){
+            String[] split = input.split(" ");
+            
+           for(String word : command){ 
+               if(input.contains(word) && split.length == 2){
+                System.out.print(split[1] + " is a shell builtin");
                 break;
-            }
-    
-            // this statement is for echo command 
-            if(input.contains("echo")){
-                String[] Array = input.split(" ");
-                for(int i = 1; i < Array.length ; i++){
-                    if(i == Array.length - 1){
-                        System.out.print(Array[i]);
-                        System.out.println();
-                        
-                    }
-                    else{
-                     System.out.print(Array[i] + " ");
-                    }
-                }
-                continue;
-             }
+               }
+               else{
+                System.out.print(split[1]+": not found");
+                break;
+               }
+           }break;
         }
         
         // this statement is to exit when the user types "exit 0"
