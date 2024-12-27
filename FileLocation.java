f09314cbd02dc847f03f9f313d0b3a8919fa01b5
Akshay <52096313+AkshayGadepally@users.noreply.github.com>
2024-12-27 23:28:20 +0530

    Add files via upload

--- FileLocation.java
+++ FileLocation.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class FileLocation{
     public static String fileLocation(String givencommand){
         String path = System.getenv("PATH");
-        String[] Directories = path.split(";");
+        String[] Directories = path.split(":");
         for(String directory : Directories){
             File file = new File(directory +File.separator+ givencommand);
             if(file.exists()&& file.canExecute()){
